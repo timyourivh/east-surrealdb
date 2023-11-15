cb1fb306eb176b513be32bde31e428abb39f36d4
Tim van Herwijnen <t.vanherwijnen@helmink.nl>
2023-11-15 17:31:56 +0100

    Update template

--- migrationTemplates/async.js
+++ migrationTemplates/async.js
@@ -1,7 +1,13 @@
+const { Surreal } = require("surrealdb.js")
+
 exports.migrate = async (db) => {
+  db = Object.assign(new Surreal(), db)
   
+  // Migration instructions
 }
 
 exports.rollback = async (db) => {
-  
+  db = Object.assign(new Surreal(), db)
+
+  // Rollback isntructions
 }
